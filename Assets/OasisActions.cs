5f8cc9eb6d36ba3c6122b9e45ab33f1c807f35e7
andywatts <awatts@paxos.com>
2022-01-15 10:42:10 -0500

    initial

---
+++ Assets/OasisActions.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OasisActions : MonoBehaviour
+{
+    private PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
+    // Game ActionMap
+    void OnJump()
+    {
+        Debug.Log("Got Jump");
+    }
+    
+    void OnOpenDashboard()
+    {
+        Debug.Log("Open Dashboard");
+        _playerInput.SwitchCurrentActionMap("Dashboard");
+    }
+    
+    void OnOpenMenu()
+    {
+        Debug.Log("Open Menu");
+        _playerInput.SwitchCurrentActionMap("Menu");
+    }
+    
+    
+    // Dashboard ActionMap
+    void OnDashboardExit()
+    {
+        Debug.Log("Get dashboard exit");
+        _playerInput.SwitchCurrentActionMap("Game");
+    }
+    
+    // Menu ActionMap
+    void OnMenuExit()
+    {
+        Debug.Log("Get menu exit");
+        _playerInput.SwitchCurrentActionMap("Game");
+    }
+    
+    
+}
